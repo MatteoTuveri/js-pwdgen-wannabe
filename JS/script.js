834b12ea1dcbd101a41e7bdd31cf1c54552e6ff2
MatteoTuveri <140177942+MatteoTuveri@users.noreply.github.com>
2023-10-06 15:25:38 +0200

    First test with JS

---
+++ JS/script.js
@@ -0,0 +1,15 @@
+const number= '21';
+
+let Name = prompt('Quale è il tuo nome?');
+let Surname = prompt('Quale è il tuo cognome?');
+let Color = prompt('Quale è il tuo colore preferito?');
+let Result= Name + Surname + Color + number;
+
+console.log(Result);
+
+document.getElementById("demo").innerHTML = Result;
+
+
+
+
+
